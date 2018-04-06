7b2479143eed81fba37446fcf08e4b686c39b745
bscherer <bscherer@MTSW10D-4T5CBM2.corp.legalmts.net>
2018-04-06 16:01:08 -0400

    heap

--- More Sorting/src/Heap.java
+++ More Sorting/src/Heap.java
@@ -1,4 +1,39 @@
+import java.util.ArrayList;
 
-public class Heap {
+public class Heap <E extends Comparable<E>>{
+	private ArrayList<E> list = new ArrayList<>();
+	
+	public Heap() {
+		
+	}
+	
+	public Heap(E[] objects) {
+		for (int i = 0; i <  objects.length; i++) {
+			add(objects[i]);
+		}
+	}
+	
+	public void add(E newObject) {
+		list.add(newObject);
+		int currentIndex = list.size() -1 ;
+		
+		while (currentIndex > 0 ) {
+			int parentIndex = (currentIndex - 1 ) / 2;
+			
+			if (list.get(currentIndex).compareTo(list.get(parentIndex)) > 0 ) {
+				E temp = list.get(currentIndex);
+				list.set(currentIndex, list.get(parentIndex));
+				list.set(parentIndex,  temp);
+			}else {
+				break;
+			}
+			currentIndex = parentIndex;
+		}
+		
+	}
 
+}
+
+public E remove() {
+	
 }
